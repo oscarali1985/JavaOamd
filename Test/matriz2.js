76c425e8ecd1a4851aafb0f164accfc06148026c
oscarali1985 <oscarali1985@hotmail.com>
2020-01-03 20:41:40 -0400

    030120

---
+++ Test/matriz2.js
@@ -0,0 +1,17 @@
+var coordinatesArray = [[33.747252,-112.633853],[-33.867886, -63.987],[41.303921, -81.901693],[-33.350534, -71.653268]];
+
+console.log(coordinatesArray);
+console.log(coordinatesArray.length);
+
+for (x=0;x<coordinatesArray.length;x++){
+        for(y=0;y<coordinatesArray.length-1;y++){
+    
+    
+        }
+        console.log(coordinatesArray[x]+""+coordinatesArray[y]);
+  }
+
+  //The first coordinate latitude
+var latitude = coordinatesArray[0][0];
+//The first coordinate longitude
+var longitude = coordinatesArray[0][1];
