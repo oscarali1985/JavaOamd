846a4920dfa564e998598fa0097eeea4ae39577a
oscarali1985 <oscarali1985@hotmail.com>
2020-01-06 23:33:38 -0400

    200106

---
+++ Test/invertirCadena.js
@@ -0,0 +1,37 @@
+//en caso de ser numero
+//nombre.toUpperCase()
+//nombre.toLowerCase()
+
+
+var cadena = "hola";
+cadena= cadena.toLowerCase()
+var counts = {};
+var repeticion=0;
+
+
+// your code here
+
+           function leerletra(cadena){
+                var caracter=[];
+                for(i=cadena.length;i>=0;i--){
+                    console.log();
+                    caracter.push(cadena.charAt(i));
+
+                        }
+                        return caracter
+                }
+
+
+            
+    
+
+
+console.log(leerletra(cadena));
+
+
+
+
+
+
+
+
