ba723ee6b607a0e773c41512c84395d21c70a03b
oscarali1985 <oscarali1985@hotmail.com>
2020-02-11 23:34:05 -0400

    Actu Ejercicio DOM

---
+++ Funciones.js
@@ -0,0 +1,8 @@
+// para eliminar espacios en blanco
+// tambien se puedes usar la funcion de js .trim()==' ' para eliminar los espacios en blancos
+
+function myTrim(x) {
+    
+    return x.replace(/^\s+|\s+$/gm,'');
+  }
+  
